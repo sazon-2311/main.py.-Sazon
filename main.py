b678775c85ddeb7103c0770f98cae6f0844fd93c
sazon <sazon9020@gmail.com>
2024-11-07 21:59:04 +0300

    1

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print(9**0.5*5) #"1st program"
-print(9.99>9.98 and 1000!=1000.1) #"2nd program"
-print(2*2+2) #"3rd program
-print(2*(2+2))
-print(2*2+2 == 2*(2+2))
-print(int(float('123.456')*10%10))
+example='Карантин'
+print(example[0])
+print(example[-1])
+print(example[3:])
+print(example[::-1])
+print(example[::2])
