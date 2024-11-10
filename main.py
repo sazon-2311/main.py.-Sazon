62050546c8cdf06af41d30c4cc7ff13a6029c10e
sazon-2311 <sazon9020@gmail.com>
2024-11-10 15:43:40 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+#базавые структуры даных
+print(9**0.5*5) #"1st program"
+print(9.99>9.98 and 1000!=1000.1) #"2nd program"
+print(2*2+2) #"3rd program
+print(2*(2+2))
+print(2*2+2 == 2*(2+2))
+print(int(float('123.456')*10%10))
+#переменные
+Number_of_completed_Dz=12
+Number_of_hours_sprint=1.5
+Course_name='Python'
+(print('Course:',Course_name,
+      'Total tasks:',Number_of_completed_Dz,
+      'hours sprint:',Number_of_hours_sprint,
+      'average compietion time:',Number_of_hours_sprint/Number_of_completed_Dz,'hours'))
+#индекс строк
 example='Карантин'
 print(example[0])
 print(example[-1])
