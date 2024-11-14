1f2f0e63e0113c598b7ac9be001e2a4ebe27784c
sazon-2311 <sazon9020@gmail.com>
2024-11-14 15:09:50 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,7 @@
-#базавые структуры даных
-print(9**0.5*5) #"1st program"
-print(9.99>9.98 and 1000!=1000.1) #"2nd program"
-print(2*2+2) #"3rd program
-print(2*(2+2))
-print(2*2+2 == 2*(2+2))
-print(int(float('123.456')*10%10))
-#переменные
-Number_of_completed_Dz=12
-Number_of_hours_sprint=1.5
-Course_name='Python'
-(print('Course:',Course_name,
-      'Total tasks:',Number_of_completed_Dz,
-      'hours sprint:',Number_of_hours_sprint,
-      'average compietion time:',Number_of_hours_sprint/Number_of_completed_Dz,'hours'))
-#индекс строк
-example='Карантин'
-print(example[0])
-print(example[-1])
-print(example[3:])
-print(example[::-1])
-print(example[::2])
+   # цели и задачи.поток выполнения програм
+print('Hi,PyCharm')
+x=43
+y=32
+print(x*y)
+print("End line")
+
