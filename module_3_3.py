e2ba5030d239a7f5e0e7a4a9dde286c59266b593
sazon-2311 <sazon9020@gmail.com>
2024-11-25 01:40:00 +0300

    Add files via upload

---
+++ module_3_3.py
@@ -0,0 +1,15 @@
+def print_params (a = 1, b = "Строка" , c = True):
+    print(a, b, c)
+
+
+print_params()
+print_params(3, 'wb', False)
+print_params(b= 25)
+print_params(c= [1,2,3])
+
+values_list = [12, "слово", True]
+values_dict = {"a" : 25, "b" : "Строка" , "c" : True }
+print_params(*values_list)
+print_params(**values_dict)
+values_list_2 = ('строка', 56)
+print_params(*values_list_2, 42)
